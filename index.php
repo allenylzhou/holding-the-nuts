31feba37d54070f5f1ad179687ae5cb56208b938
Allen Zhou <allenylzhou@gmail.com>
2013-11-02 18:20:39 -0700

    Added model class for Game, implemented select all

--- index.php
+++ index.php
@@ -1,32 +1,40 @@
 <html>
 <body>
-<h1>It works!</h1>
+<h1>Welcome to 304!</h1>
+
+<h2>Function 1: Show Game History by User ID</h2>
 
 <form method="POST" action="index.php">
-	<p>
-		<label>Time In</label>
-		<input type="text" name="timeIn" size="6">
-	</p>
-	<p>
-		<label>Buy In</label>
-		<input type="text" name="buyIn" size="6">
-	</p>
-	<p>
-		<label>Time Out</label>
-		<input type="text" name="timeOut" size="6">
-	</p>
-	<p>
-		<label>Buy Out</label>
-		<input type="text" name="buyOut" size="6">
-	</p>
-	<p><input type="submit" value="Record" name="record"></p>
+	<label>User_Id:</label>
+	<input type="text" name="userId" value="0" size="1">
+	<input type="submit" value="Cash Games" name="cash">
+	<input type="submit" value="Tournament Games" name="tournament">
 </form>
+
+<h2>Function 2: Create Game Session</h2>
+...
 
 <?php
 
-	if (array_key_exists('record', $_POST)) {
-		echo "Recorded!";
+include 'tbs_class.php';
+include 'game_class.php';
+
+	if (array_key_exists('cash', $_POST) || array_key_exists('tournament', $_POST)) {
+
+		$gameType = array('cash' => isset($_POST['cash']), 'tournament' => isset($_POST['tournament']));
+
+		if($gameType['cash']) {
+			$gameData = CashGame::loadSavedGames($_POST['userId']);
+		} else {
+			$gameData = TournamentGame::loadSavedGames($_POST['userId']);
+		}
+
+		$TBS = new clsTinyButStrong;
+		$TBS->LoadTemplate('game-history.htm');
+		$TBS->MergeBlock('gameData', $gameData);
+		$TBS->Show();
 	}
+
 ?>
 	
 </body>
