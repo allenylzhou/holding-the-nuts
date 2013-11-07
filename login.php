3fb86fb189834c884fb547823f65f2882af555f9
yjeffrey <jeff_8514@hotmail.com>
2013-11-07 00:30:04 -0800

    forgot a file

---
+++ login.php
@@ -0,0 +1,53 @@
+<html>
+<body>
+<h1>Login</h1>
+<form method="POST" action="login.php">
+	<label>Username:</label>
+	<input type="text" name="username">
+	<label>Password:</label>
+	<input type="password" name="password">
+	<input type="submit" name="" value="Login" >
+</form>
+
+<?php
+
+include 'user_class.php';
+include 'tbs_class.php';
+	if (!array_key_exists('username', $_POST) && !array_key_exists('password', $_POST)) {
+		
+	}
+	else if (!array_key_exists('username', $_POST) || !array_key_exists('password', $_POST)) {
+		echo 'Please fill in the Login form.';
+	}
+	else if($_POST['username'] == '' || $_POST['password'] == ''){
+		echo 'Please fill in all of the form please';
+	}
+	else {
+		$connection;
+		try {
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			
+			$user = new User;
+			$user->setProperties(array(
+				'username' => $username,
+				'password' => $password
+			));
+			
+			$user->login();
+			echo 'login successful';
+		}
+		catch (Exception $exception) {
+			echo $exception->getMessage();
+		}
+	}
+	$TBS = new clsTinyButStrong;
+	//$TBS->LoadTemplate('game-history.html');
+	//$TBS->MergeBlock('gameData', $gameData);
+	$TBS->Show();
+?>
+
+
+	
+</body>
+</html>
