939fdf82df2c7fb5725a6b04b43ca7b82521e1ed
Nathan Chow <nathanychow@gmail.com>
2013-11-08 19:04:50 -0800

    first statistics

---
+++ statistics.php
@@ -0,0 +1,37 @@
+<?php
+
+include 'database_class.php';
+
+class Statistics extends Database{
+
+	public static function getAverageCashBuyIn($userId){
+		if ($connection = oci_connect("ora_u4e7", "a71174098", "ug")){
+			$sqlString = 'SELECT AVG(AMOUNT_IN) 
+				FROM Game G, Game_Cash C
+				Where G.gs_id = C.gs_id And G.user_id = (:userID)';
+		
+			$sqlStatement = oci_parse($connection, $sqlString);
+			oci_bind_by_name($sqlStatement, ':userId', $userId);
+			
+			oci_execute($sqlStatement);
+			
+			$returnData = array();
+			while($row = oci_fetch_array($sqlStatement)){
+			
+				array_push($returnData, $row);
+			}
+			OCILogoff($connection);
+		}else{
+			$err = OCIError();
+			echo "Oracle Connect Error " . $err['message'];
+			
+		}
+	
+		return $returnData;
+	}
+
+
+
+}
+
+?>
