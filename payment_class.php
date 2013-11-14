0917587cdfc8045756e43153e314f4262f56f948
yjeffrey <jeff_8514@hotmail.com>
2013-11-14 10:14:01 -0800

    added in extra view for backing, so it's cleaner

--- payment_class.php
+++ payment_class.php
@@ -7,21 +7,22 @@ class PaymentPart extends Database {
 	// This maps model properties to database
 	protected static $tableSchemas = array(
 		'GAME' => array(
-			'ppID' => 'PP_ID',
+			'baId' => 'BA_ID',
 			'gsId' => 'GS_ID',
+			'paymentSubpart' => 'PAYMENT_SUBPART',
+			'paymentDate' => 'PAYMENT_DATE',
 			'amount' => 'AMOUNT'
 		)
 	);
-
-	protected static $tableSequencer = 'PAYMENT_PART_SEQUENCE';
 
 	protected function __construct () {
 		parent::__construct();
 	}
 
-	protected $ppID;
+	protected $baId;
 	protected $gsId;
 	protected $paymentSubpart;
+	protected $paymentDate;
 	protected $amount;
 
 	public function setProperties($properties) {
@@ -31,7 +32,7 @@ class PaymentPart extends Database {
 	}
 
 	public function save() {
-		if (isset($this->ppID)) {
+		if (isset($this->baId)) {
 			$this->update();
 		} else {
 			$this->insert();
